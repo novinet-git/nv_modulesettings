e824ff4ce5e73cb137ae0e31bd3fe5a8d3089029
Daniel Steffen <57093581+novinet-dsteffen@users.noreply.github.com>
2021-11-27 20:03:40 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -6,7 +6,7 @@ if (rex::isBackend())
     rex_view::addJsFile($this->getAssetsUrl('script.js?v=' . $this->getVersion()));
     rex_view::addCssFile($this->getAssetsUrl('bootstrap-slider.min.css?v=' . $this->getVersion()));
     rex_view::addCssFile($this->getAssetsUrl('style.css?v=' . $this->getVersion()));
-    rex_view::addJsFile($addon->getAssetsUrl('rangeInput.js'));
+    rex_view::addJsFile($this->getAssetsUrl('rangeInput.js'));
 
     $oSettings = new nvModuleSettings;
     $oSettings->syncWithThemeAddon();
